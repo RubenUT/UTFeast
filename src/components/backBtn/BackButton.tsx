a9f7d336d7e82749590fc0a9fc98a3c327c821d6
RubenUT <21354385@utcancun.edu.mx>
2024-10-18 14:47:53 -0600

    style: Register.tsx
    - Modificar estructura de Register.tsx para estructurarlo por contenedores.
    - Modificar estilos de Register.css para seguir la metodologia BEM

--- src/components/backBtn/BackButton.tsx
+++ src/components/backBtn/BackButton.tsx
@@ -2,6 +2,7 @@ import React from 'react';
 import { useHistory } from 'react-router-dom';
 import { IonButton, IonIcon } from '@ionic/react';
 import { arrowBackOutline } from 'ionicons/icons';
+import './BackButton.css';
 
 const BackButton: React.FC = () => {
   const history = useHistory();
